fc60f62efba32ac0c522fa5446c3012a70750275
LuaMartz <luisamartinez030503@gmail.com>
2023-07-05 16:45:30 -0500

    modified:   controllers/author.js

--- controllers/author.js
+++ controllers/author.js
@@ -1,18 +1,22 @@
 const Author = require('../models/author') 
 
 const controllerAuthor = {
-    create : async (req,res) => {
-        try {
-            const first_name = req.body.first_name
-            const family_name = req.body.family_name
-            await Author.create({
-                first_name : first_name,
-                family_name : family_name
-            }).then(console.log('Author created'))
-        } catch {
-            return res.status(500).json({msg:error.message})
-        }
+    create: async (req, res) => {
+      try {
+        const first_name = req.body.first_name;
+        const family_name = req.body.family_name;
+        await Author.create({
+          first_name: first_name,
+          family_name: family_name
+        });
+        console.log('Author created');
+        res.status(200).json({ message: 'Author created' });
+      } catch (error) { // <-- Add the error parameter here
+        console.error(error);
+        return res.status(500).json({ msg: error.message });
+      }
     }
-}
-
-module.exports = controllerAuthor
+  };
+  
+  module.exports = controllerAuthor;
+  
